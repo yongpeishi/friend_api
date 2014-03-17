387195805443013eca95b5cba5ce4ebf4861623c
yongpeishi <yongpeishi@gmail.com>
2014-03-17 16:51:52 +1100

    return json response consist of 1 result

--- src/friend_api/handler.clj
+++ src/friend_api/handler.clj
@@ -1,12 +1,16 @@
 (ns friend-api.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [cheshire.core :refer :all]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" []
+    (generate-string {:name "Big Teddy" :desc "Big and fluffy"}))
   (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
   (handler/site app-routes))
+
+(defn list-of-friends [])
